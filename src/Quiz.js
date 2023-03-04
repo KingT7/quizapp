7d6388292445be9913cf9b799a0052e730847e79
Nicholas H <NicholasH7@hotmail.com>
2023-03-03 20:39:27 -0500

    completed coding

---
+++ src/Quiz.js
@@ -0,0 +1,76 @@
+export const quiz = {
+    topic: 'Random Questions',
+    level: 'Insane',
+    totalQuestions: 10,
+    perQuestionScore: 10,
+    questions: [
+      {
+        question:
+          'How many ribs are in a human body?',
+        choices: ['15', '3', '24', '34'],
+        type: 'MCQs',
+        correctAnswer: '24',
+      },
+      {
+        question:
+          'What color eyes do most humans have?',
+        choices: ['Green', 'Hazel', 'Blue', 'Brown'],
+        type: 'MCQs',
+        correctAnswer: 'Brown',
+      },
+      {
+        question:
+          'Which name is rapper Sean Combs better known by?',
+        choices: [
+          'P. Diddy',
+          'Lil Baby',
+          'Ja Rule',
+          'None of The Above',
+        ],
+        type: 'MCQs',
+        correctAnswer: 'P. Diddy',
+      },
+      {
+        question: 'How many hearts does an octopus have?',
+        choices: ['4', '1', '3', '5'],
+        type: 'MCQs',
+        correctAnswer: '3',
+      },
+      {
+        question: 'Power outages in the US are mostly caused by what?',
+        choices: ['Weather', 'Squirrels', 'Birds', 'Cats'],
+        type: 'MCQs',
+        correctAnswer: 'Squirrels',
+      },
+      {
+        question: 'What was the first state?',
+        choices: ['Delaware', 'New York', 'Florida', 'Ohio'],
+        type: 'MCQs',
+        correctAnswer: 'Delaware',
+      },
+      {
+        question: 'Water has a pH level of around?',
+        choices: ['2', '10', '4', '7'],
+        type: 'MCQs',
+        correctAnswer: '7',
+      },
+      {
+        question: 'What is the most common letter in the English alphabet?',
+        choices: ['E', 'L', 'C', 'A'],
+        type: 'MCQs',
+        correctAnswer: 'E',
+      },
+      {
+        question: 'What object is said to bring bad luck if it is broken?',
+        choices: ['Cup', 'Plate', 'Mirror', 'Televison'],
+        type: 'MCQs',
+        correctAnswer: 'Mirror',
+      },
+      {
+        question: 'What is the first element on the Periodic Table?',
+        choices: ['Sodium', 'Hydrogen', 'Calcium', 'Carbon'],
+        type: 'MCQs',
+        correctAnswer: 'Hydrogen',
+      },
+    ],
+  }
